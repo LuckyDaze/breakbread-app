3392a42a5b738ff6fa887aa0527cf21fa3ea3136
LuckyDaze <larsendjery@yahoo.com>
2025-10-05 06:55:22 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,8 @@ def format_currency(amount):
 
 def safe_get(d, key, default=None):
     return d[key] if key in d else default
+
+def percent_change(old, new):
+    if old == 0:
+        return 0.0
+    return ((new - old) / old) * 100
