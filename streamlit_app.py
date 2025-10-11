9558ee50eaef5a77e0c5eb019c27df572c6a4e19
LuckyDaze <larsendjery@yahoo.com>
2025-10-10 23:23:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,7 +204,7 @@ st.markdown("""
 # ----------------------------
 st.set_page_config(
     page_title="Break Bread",
-    page_icon="🍞",
+    page_icon="assets/favicon.png",
     layout="wide",
     initial_sidebar_state="expanded",
 )
